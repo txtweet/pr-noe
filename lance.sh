199950638896bca612b6eec0a85b3fd09c336f64
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2020-10-25 15:08:30 +0100

    Modification lance

--- lance.sh
+++ lance.sh
@@ -1,19 +1,22 @@
 while true; do
-  echo "Tick"
-  coffee ./crawl.coffee > cryptos2.json
-  cp ./cryptos2.json cryptos.json
-  # git add cryptos.json
-  sleep 60
+  #echo "Tick"
+  #coffee ./crawl.coffee > cryptos2.json
+  #cp ./cryptos2.json cryptos.json
+  ## git add cryptos.json
+  #sleep 60
 
   echo "Token"
   coffee ./crawlTokens.coffee > tokens.json
   cp ./tokens.json cryptos.json
 
-  sleep 15
-
   echo "Token2"
   coffee ./crawlTokens.coffee > tokens.json
   cp ./tokens.json cryptos.json
+
+  #echo "Token3"
+  #coffee ./crawlTokens.coffee > tokens.json
+  #cp ./tokens.json cryptos.json
+
   sleep 60
 
 done
