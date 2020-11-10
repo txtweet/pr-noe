8ce9c21868ce5b2be9a4081e1fd45ab30a3d56ef
TxTweet <nonogerboy@gmail.com>
2020-11-10 19:37:18 +0100

    Test Treemap + coffee

--- cryptostoD3js.js
+++ cryptostoD3js.js
@@ -3,42 +3,6 @@ const fs = require('fs')
 
 result = {}
 
-function sort_tag_old(data){
-    var tags={}
-    Object.keys(data).forEach((x)=>{
-        data[x]["tags"].forEach(tag => {
-            if(tag in tags){
-                tags[tag]+=1
-            }else{
-                tags[tag]=1
-            }
-        })
-    })
-    if(Object.keys(tags).length===0){
-        var ret= {"name":tags[0],"children":[]}
-        Object.keys(data).forEach((x)=>{
-            ret["children"].push({"name":x,"value":100})
-        })
-        return ret
-    }else{
-        var tab_tags = Object.entries(tags).sort((a,b)=>(b[1]-a[1]))
-        console.log(tab_tags)
-        if(tab_tags.length===0){
-            return data
-        }
-        var soustab={}
-        Object.keys(data).forEach((x)=>{
-            if (data[x]["tags"].includes(tab_tags[0][0])){
-                soustab[x]=data[x]
-                soustab[x]["tags"].splice(soustab[x]["tags"].indexOf(tab_tags[0][0]),1)
-                delete data.x
-            }
-        })
-        return {"name":tab_tags[0][0],"children":[sort_tag(soustab)]}
-    }
-
-}
-
 function extract_data(keyword, data){
     const ret = {"name" : keyword,
     "children" :[]}
@@ -81,9 +45,8 @@ function extract_data(keyword, data){
 }
 
 const jsonformat = extract_data("root",datas)
-jsonformat
 console.log(jsonformat)
-jsontos = JSON.stringify(jsonformat, null, 4)
-fs.writeFile("extracted.json", jsontos, (err) => console.log(err))
+jsontos = JSON.stringify(jsonformat, null, 2)
+fs.writeFile("flare-2.json", jsontos, (err) => console.log(err))
 //console.log(datas)
 //console.log(res)
