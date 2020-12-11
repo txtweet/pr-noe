c07b3a1d5033d0016da50dc83edc8ce04eca86b0
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2020-12-11 11:54:04 +0100

    Decoration

--- crawlers/lance.sh
+++ crawlers/lance.sh
@@ -1,31 +1,6 @@
 while true; do
-  #echo "Tick"
-  #coffee ./crawl.coffee > cryptos2.json
-  #cp ./cryptos2.json cryptos.json
-  ## git add cryptos.json
-  #sleep 60
 
-  # echo "Token"
-  # coffee ./crawlTokens.coffee > tokens.json
-  # cp ./tokens.json cryptos.json
-  #
-  # echo "Token2"
-  # coffee ./crawlTokens.coffee > tokens.json
-  # cp ./tokens.json cryptos.json
-
-  #echo "Token3"
-  #coffee ./crawlTokens.coffee > tokens.json
-  #cp ./tokens.json cryptos.json
-
-  # echo "Ftx"
-  # coffee ./crawlFtx.coffee > tokens.json
-  # cp ./tokens.json ../cryptos.json
-  #
-  # echo "Ftx"
-  # coffee ./crawlFtx.coffee > tokens.json
-  # cp ./tokens.json ../cryptos.json
-
-  echo "New crypto 1/2"
+  echo "Search crypto 1/2"
   coffee ./updateNew2WithCrawl.coffee > tokens.json
   ret=$?
   cp ./tokens.json ../cryptos.json
@@ -34,7 +9,7 @@ while true; do
     break
   fi
 
-  echo "New crypto 2/2"
+  echo "Search crypto 2/2"
   coffee ./updateNew2WithCrawl.coffee > tokens.json
   ret=$?
   cp ./tokens.json ../cryptos.json
