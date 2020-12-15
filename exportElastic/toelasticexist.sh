10e2d4cc7edc7b6d3690b2b3e991df8b80d90dc0
Noé G <nonogerboy@gmail.com>
2020-12-15 18:02:07 +0100

    Still Some issues

--- exportElastic/toelasticexist.sh
+++ exportElastic/toelasticexist.sh
@@ -3,7 +3,7 @@ cd ./exportElastic/
 
 for date in '01122020' '03122020' '07122020' '08122020' '09122020' '11122020' '13122020' '14122020' '15122020'; do
   echo $date
-  coffee cryptosToElastic.coffee $date > data_elastic.json
-  curl -XPUT 192.168.1.136:9200/_bulk -H"Content-Type: application/json" --data-binary @data_elastic.json > ans_$date.log
-  rm  data_elastic.json
+  coffee cryptosToElastic.coffee $date > data_elastic_$date.json
+  curl -XPUT 192.168.1.136:9200/_bulk -H"Content-Type: application/json" --data-binary @data_elastic_$date.json > ans_$date.log
+  #rm  data_elastic.json
 done
