47e79675e572c3937079ba044e2ccaf254d0040a
Noé G <nonogerboy@gmail.com>
2020-12-16 16:03:55 +0100

    Init done

--- exportElastic/initCollection.sh
+++ exportElastic/initCollection.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ad='192.168.1.136:9200'
-echo curl --location --request PUT "'$ad/cryptos'"
-echo curl --location --request PUT "'$ad/cryptos/_settings'" --header 'Content-Type: application/json' --data-raw '{
+curl --location --request PUT "$ad/cryptos" > init.log
+curl --location --request PUT "$ad/cryptos/_settings" --header 'Content-Type: application/json' --data-raw '{
   "index.mapping.total_fields.limit": 5000
-}'
+}' >> init.log
