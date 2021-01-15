b597b70624781d9183af347b802165545b743348
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2021-01-15 17:33:38 +0100

    Ajout cryptos

--- crawlers/lanceDownload.sh
+++ crawlers/lanceDownload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "Récupération des données dans crypto-$*.json"
 
 set `date +%d%m%Y`
+echo "Récupération des données dans crypto-$*.json"
 
 curl -sS "https://web-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?aux=circulating_supply,tags,max_supply,total_supply&convert=USD&cryptocurrency_type=all&limit=5000&sort=market_cap&sort_dir=desc&start=1"|json > ../cryptos-$*.json
 echo "git add ../cryptos-$*.json && git commit -am'Ajout cryptos' && git push"
