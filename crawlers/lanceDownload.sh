0674317add2507c71b08ab567f06d2fca941a2d9
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2021-07-21 10:28:40 +0200

    Ajout crypto date

--- crawlers/lanceDownload.sh
+++ crawlers/lanceDownload.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-#
-# set `date +%Y%m%d`
-# echo "Récupération des données dans ../files/cryptos-$*.json"
-#
-# curl -sS "https://web-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?aux=circulating_supply,tags,max_supply,total_supply&convert=USD&cryptocurrency_type=all&limit=5000&sort=market_cap&sort_dir=desc&start=1"|json > ../files/cryptos-$*.json
-# echo "git add ../files/cryptos-$*.json && git commit -am'Ajout cryptos' && git push"
-# echo "coffee ./addNewCryptosFromFreshData.coffee $* > toto.json"
-# echo "mv ./toto.json ../cryptos.json"
-# echo "git add ../cryptos.json && git commit -am'Ajout cryptos' && git push"
-# cd files
+
+array=(`cat ./cryptodate.lst`)
+
+for file in ../files/cryptos-*.json ; do
+  set `echo "$file"|cut -d '-' -f 2|cut -d '.' -f 1` # Remove - and . from file
+  value=$*
+  if [[ ! " ${array[@]} " =~ " ${value} " ]]; then
+    echo "Date $*"
+    coffee ./addNewCryptosFromFreshData.coffee $* > toto.json
+    ret=$?
+    if [[ $ret -gt 0 ]]; then
+      echo "Commande à relancer"
+      exit 1
+    fi
+    cp ./toto.json ../cryptos.json
+    echo "$*" >> cryptodate.lst
+  fi
+done
+echo "Terminé"
